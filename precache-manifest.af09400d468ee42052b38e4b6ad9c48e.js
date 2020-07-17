89f75b1811165d7c2bd6eb23581658a1889c4d23
sumancfc <sumanstha999@gmail.com>
2020-07-17 20:28:47 +0545

    Updates

---
+++ precache-manifest.af09400d468ee42052b38e4b6ad9c48e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f1513657d083c8af1e6d4263b0ccd6",
+    "url": "/gitfinder/index.html"
+  },
+  {
+    "revision": "de95b2a6a13e74a77283",
+    "url": "/gitfinder/static/css/main.3b010753.chunk.css"
+  },
+  {
+    "revision": "a1766262bd3e718ac631",
+    "url": "/gitfinder/static/js/2.13defe9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/gitfinder/static/js/2.13defe9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de95b2a6a13e74a77283",
+    "url": "/gitfinder/static/js/main.c5b1d0db.chunk.js"
+  },
+  {
+    "revision": "96204fefe74ca87209e0",
+    "url": "/gitfinder/static/js/runtime-main.781843ed.js"
+  },
+  {
+    "revision": "8e505e42379328dcea6f811d05283416",
+    "url": "/gitfinder/static/media/spinner.8e505e42.gif"
+  }
+]);
